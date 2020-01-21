9451b72a0f4c2e534f3feecb3306675736947fe1
Sai Siddartha Maram <smaram_be16@thapar.edu>
2020-01-21 16:23:37 +0530

    Detection

--- RP1/Code/constants.py
+++ RP1/Code/constants.py
@@ -7,6 +7,9 @@ ALT_REF = 450
 #Object detection model settings for yolo/tiny-yolo
 options = {
                 "model": "darkflow/cfg/yolo.cfg",
-                "load": "darkflow/bin/yolo.weights",
+                "load": "darkflow/bin/yolov2.weights",
                 "threshold": 0.50,
+                "confidence":0.3,
+                "labelsPath":"darkflow/coco.names"
             }
+            
--- RP1/Code/detect.py
+++ RP1/Code/detect.py
@@ -5,24 +5,24 @@ import cv2
 import os
 
 
-np.random.seed(42)
 
-labelsPath = "darkflow/coco.names"
-LABELS = open(labelsPath).read().strip().split("\n")
 
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
-	dtype="uint8")
-#Object detection model settings for yolo/tiny-yolo
 options = {
                 "model": "darkflow/cfg/yolo.cfg",
                 "load": "darkflow/bin/yolov2.weights",
                 "threshold": 0.50,
+                "confidence":0.3,
+                "labelsPath":"darkflow/coco.names"
             }
+            
+np.random.seed(42)
+LABELS = open(options['labelsPath']).read().strip().split("\n")
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
 
 net = cv2.dnn.readNetFromDarknet(options['model'], options['load'])
 
 image=cv2.imread('00001.jpg')
-image=cv2.resize(image,(900,900))
+image=cv2.resize(image,(900,450))
 
 (H, W) = image.shape[:2]
  
@@ -42,52 +42,30 @@ boxes = []
 confidences = []
 classIDs = []
 for output in layerOutputs:
-    # loop over each of the detections
     for detection in output:
-        # extract the class ID and confidence (i.e., probability)
-        # of the current object detection
         scores = detection[5:]
         classID = np.argmax(scores)
         confidence = scores[classID]
-
-        # filter out weak predictions by ensuring the detected
-        # probability is greater than the minimum probability
-        if confidence > 0.3:
-            # scale the bounding box coordinates back relative to
-            # the size of the image, keeping in mind that YOLO
-            # actually returns the center (x, y)-coordinates of
-            # the bounding box followed by the boxes' width and
-            # height
+        if confidence > options['confidence']:
             box = detection[0:4] * np.array([W, H, W, H])
             (centerX, centerY, width, height) = box.astype("int")
-
-            # use the center (x, y)-coordinates to derive the top
-            # and and left corner of the bounding box
             x = int(centerX - (width / 2))
             y = int(centerY - (height / 2))
-
-            # update our list of bounding box coordinates,
-            # confidences, and class IDs
             boxes.append([x, y, int(width), int(height)])
             confidences.append(float(confidence))
             classIDs.append(classID)
 
-idxs = cv2.dnn.NMSBoxes(boxes, confidences,0.5,0.3)
+idxs = cv2.dnn.NMSBoxes(boxes, confidences,options['confidence'],options['threshold'])
 
-# ensure at least one detection exists
 if len(idxs) > 0:
-	# loop over the indexes we are keeping
 	for i in idxs.flatten():
 		# extract the bounding box coordinates
 		(x, y) = (boxes[i][0], boxes[i][1])
 		(w, h) = (boxes[i][2], boxes[i][3])
- 
-		# draw a bounding box rectangle and label on the image
 		color = [int(c) for c in COLORS[classIDs[i]]]
 		cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
 		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
 		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,0.5, color, 2)
  
-# show the output image
 cv2.imshow("Image", image)
 cv2.waitKey(0)
