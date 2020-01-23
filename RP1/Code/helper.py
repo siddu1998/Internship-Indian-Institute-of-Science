52a17d411ad49168a1a9ccf8b3671dc522195568
Sai Siddartha Maram <smaram_be16@thapar.edu>
2020-01-23 15:16:10 +0530

    helper

--- RP1/Code/helper.py
+++ RP1/Code/helper.py
@@ -82,12 +82,16 @@ def batch_detection(query_list):
 
 
 
-def break_video_to_frame(input_path,output_path):
-    vidcap = cv2.VideoCapture(input_path_video)
+def break_video_to_frame(input_path):
+    vidcap = cv2.VideoCapture(input_path)
     success,image = vidcap.read()
     count = 0
     while success:
-    cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
-    success,image = vidcap.read()
-    print('Read a new frame: ', success)
-    count += 1
+        cv2.imwrite("{}.jpg".format(count), image)     # save frame as JPEG file      
+        success,image = vidcap.read()
+        print('Read a new frame {}: '.format(count), success)
+        count += 1
+
+print('[INFO] Starting process')
+break_video_to_frame('/home/sid/Desktop/research_videos/VIRAT_S_000001.mp4')
+print('[INFO] Finished Conversion')
